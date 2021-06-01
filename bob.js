09df592bd39126ce405631ba094c5d4a6527ebf4
Raina-2007 <75320146+Raina-2007@users.noreply.github.com>
2021-06-01 17:19:53 +0530

    Update bob.js

--- bob.js
+++ bob.js
@@ -1,7 +1,7 @@
 class Bob{
     constructor(x,y){
         var options = {
-            restitution : 0.3,
+            restitution : 1.0,
             friction : 0.5,
             density : 1.2,
             isStatic : false
